f76cd50f73d8d010c21f8cccbb10fafede863611
oshri2828-byte <oshri2828@gmail.com>
2026-02-05 15:50:47 +0200

    transfer moving artifact from job to job

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return jsonify(
-        message="Hello from Flask in Docker 🚀",
+        message="Hello from Flask app",
         status="ok"
     )
 
